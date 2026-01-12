69e4c5261fff259b2a8ff49606bb727061d38840
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-01-12 12:48:38 +0530

    DAY 6

---
+++ Day 6/Greeting App/program.cs
@@ -0,0 +1,21 @@
+﻿using Greeting_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting_App
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+            string greeting = GreetingHelper.GetGreeting(name);
+            Console.WriteLine(greeting);
+        }
+    }
+}
---
+++ Day 6/Greeting Library/GreetingHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting_Library
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return "Hello, Guest!";
+            }
+            return $"Hello, {name}!";
+        }
+    }
+}
