18274b9150d7d41e583e7f4194f4ee769583a072
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-01-14 14:19:03 +0530

    DAY 8

---
+++ DAY 8/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LoginMessageProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+           
+            string[] parts = input.Split('|');// Splitting
+            string user = parts[0].Trim();
+            string message = parts[1].Trim().ToLower();
+
+            string standard = "login successful";//messages to be displayed
+            string status;
+            if (!message.Contains("successful"))
+                status = "LOGIN FAILED";
+            else if (message == standard)
+                status = "LOGIN SUCCESS";
+            else
+                status = "LOGIN SUCCESS (CUSTOM MESSAGE)";
+
+            Console.WriteLine("User     : " + user);// output
+            Console.WriteLine("Message  : " + message);
+            Console.WriteLine("Status   : " + status);
+
+        }
+    }
+}
+
