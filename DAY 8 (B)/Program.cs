7f40223841f491cef13e24b3449e5ec4605942e6
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-01-16 10:13:35 +0530

    DAY 8(B)

---
+++ DAY 8 (B)/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Bank_Transaction_Narration_Analyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();  
+            string[] data = input.Split('#');
+
+            string transactionId = data[0];
+            string accountHolder = data[1];
+            string narration = data[2];
+
+            narration = narration.Trim();
+
+            while(narration.Contains(" "))
+            {
+                narration = narration.Replace("  ", " ");
+            }
+
+            narration = narration.ToLower();
+
+            bool hasDeposit = narration.Contains("deposit");
+            bool hasWithdrawal = narration.Contains("withdrawal");
+            bool hasTransfer = narration.Contains("transfer");
+
+            bool hasKeyword = hasDeposit || hasWithdrawal || hasTransfer;
+
+            string standardNarration = "cash deposit successful";
+            bool isStandard = narration.Equals(standardNarration);
+
+            string category;
+            if(!hasKeyword)
+            {
+                category = "Non Financial transaction";
+            }
+            else if(hasKeyword && isStandard)
+            {
+                category = "Standard transaction";
+            }
+            else
+            {
+                category = "Custom transaction";
+            }
+
+            Console.WriteLine("Transaction ID:" + transactionId);
+            Console.WriteLine("Account Holder :" + accountHolder);
+            Console.WriteLine("Narration:" + narration);
+            Console.WriteLine("Category:" + category);
+
+        }
+    }
+}
