cc8feb70f1ac8380e86e603ac2be5e3501e14d80
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-02-04 15:26:03 +0530

    DAY 25-1

---
+++ WEEK 5/DAY 25-1/SecureTerminal.cs
@@ -0,0 +1,24 @@
+﻿namespace Week_5
+{
+    internal class SecureTerminal
+    {
+        static void Main(string[] args)
+        {
+            string pin = "";
+            Console.WriteLine("Enter the secret 4-digit pin:");
+
+            while(pin.Length<4)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                if(char.IsDigit(keyInfo.KeyChar) )
+                {
+                   pin += keyInfo.KeyChar;
+                    Console.Write("*");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Your secret pin is :{pin} ");
+        }
+    }
+}
