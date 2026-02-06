f38887d07a69a7b9c916af9e0e3af4b9676e7206
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-02-06 11:44:56 +0530

    exercises added

---
+++ WEEK 5/DAY 27-1/DailyLogger.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Week_5
+{
+    internal class DailyLogger
+    {
+        static void Main(string[] args)
+        {
+            string directory = @"../../../";
+            if(!Directory.Exists(directory))
+            {
+                Console.WriteLine("Directory does not exist");
+                return;
+            }
+
+            string file = "journal.txt";
+            string path = directory + file;
+
+            using StreamWriter sw = new StreamWriter(path, true);
+
+            do
+            {
+                Console.WriteLine("Enter the daily data");
+                string data = Console.ReadLine();
+                if (data == "stop")
+                    break;
+                sw.WriteLine(data);
+
+                DateTime today = DateTime.Today; 
+                sw.WriteLine($"{today:dd-MM-yyyy} : {data}");
+            } while (true);
+        }
+    }
+}
