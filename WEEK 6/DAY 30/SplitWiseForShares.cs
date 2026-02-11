c6bd53128ce5dc026a78a777b352171892797a2a
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-02-11 12:45:21 +0530

    DAY 30 ADDED

---
+++ WEEK 6/DAY 30/SplitWiseForShares.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Class_Practice
+{
+    internal class SplitWiseForShares
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the number of total people:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double[] paid = new double[n];
+            double total = 0;
+
+            for (int i = 0; i < n; i++)// humne total nikala
+            {
+                Console.WriteLine($"Enter the amount paid{i+1}: ");
+                paid[i]  = Convert.ToDouble(Console.ReadLine());
+                total += paid[i];
+            }
+
+            double share = total / n;//then kitna kitna share hoga per head
+            Console.WriteLine($"Total share per head is{share}");
+
+
+            double[] diff = new double[n];//then difference ki kisko kitna dena h ya lena h
+            for (int i = 0; i < n; i++)
+            {
+                diff[i] = paid[i] - share;   
+            }
+
+            Console.WriteLine("Who pays whom:");
+
+            for (int i = 0; i < n; i++)//jisko money deni hogi
+            {
+                if (diff[i] < 0) 
+                {
+                    for (int j = 0; j < n; j++)//jisko receive krni h 
+                    {
+                        if(diff[j] > 0)
+                        {
+                            double amount;
+                            if (-diff[i] < diff[j])
+                            {
+                                amount = -diff[i];   // jitna i dega
+                            }
+                            else
+                            {
+                                amount = diff[j];    // jitna j ko receive krna h
+                            }
+
+                            Console.WriteLine($"Person {i + 1} pays {amount} to Person {j + 1}");
+
+                            diff[i] += amount;//jitna diya vo km krna h and jitna liya vo add 
+                            diff[j] -= amount;
+
+                            if (diff[i] == 0) //-------------if paid amount= share then break the loop
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Splitting is done");
+        }
+    }
+}
---
+++ WEEK 6/DAY 30/Splitwise.cs
@@ -0,0 +1,95 @@
+﻿namespace WEEK_6
+{
+    internal class Splitwise
+    {
+        static List<string> SettleExpenseShare(Dictionary<string, double> expenses)
+        {
+            List<string> settlement = new List<string>();
+
+            // Queues
+            Queue<KeyValuePair<string, double>> receivers =
+                new Queue<KeyValuePair<string, double>>();
+
+            Queue<KeyValuePair<string, double>> payers =
+                new Queue<KeyValuePair<string, double>>();
+
+            var totalExpense = expenses.Values.Sum();
+            var persons = expenses.Count;
+            var share = totalExpense / persons;
+
+            // Populate queues
+            foreach (var person in expenses)
+            {
+                if (person.Value < share)
+                {
+                    // Person needs to PAY
+                    payers.Enqueue(
+                        new KeyValuePair<string, double>(
+                            person.Key,
+                            share - person.Value));
+                }
+                else if (person.Value > share)
+                {
+                    // Person needs to RECEIVE
+                    receivers.Enqueue(
+                        new KeyValuePair<string, double>(
+                            person.Key,
+                            person.Value - share));
+                }
+            }
+
+            // Settlement
+            while (payers.Count > 0 && receivers.Count > 0)
+            {
+                var payer = payers.Dequeue();
+                var receiver = receivers.Dequeue();
+
+                double amount = Math.Min(payer.Value, receiver.Value);
+
+                settlement.Add($"{payer.Key}, {receiver.Key}, {amount}");
+
+                // Remaining balances
+                if (payer.Value > amount)
+                {
+                    payers.Enqueue(
+                        new KeyValuePair<string, double>(
+                            payer.Key,
+                            payer.Value - amount));
+                }
+
+                if (receiver.Value > amount)
+                {
+                    receivers.Enqueue(
+                        new KeyValuePair<string, double>(
+                            receiver.Key,
+                            receiver.Value - amount));
+                }
+
+                Console.WriteLine("settlement done");
+            }
+
+            return settlement;
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> expenses =
+                new Dictionary<string, double>()
+                {
+                    {"Varsha" ,100},
+                    {"kriti", 200},
+                    {"suraj", 300},
+                    {"duggu",400 }
+                };
+
+            List<string> settlement = SettleExpenseShare(expenses);
+
+            Console.WriteLine("\nFinal Settlement:");
+
+            foreach (var payment in settlement)
+            {
+                Console.WriteLine(payment);
+            }
+        }
+    }
+}
