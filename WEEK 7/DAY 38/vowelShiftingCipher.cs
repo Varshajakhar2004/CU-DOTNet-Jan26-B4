412724aec5e0ca0034a00b14a3465e8c87c49619
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-02-26 20:54:13 +0530

    DAY 38

---
+++ WEEK 7/DAY 38/vowelShiftingCipher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace Class_Practice
+{
+    internal class vowelShiftingCipher
+    {
+        public string VowelShiftCipher(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return "";
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (char ch in s)
+            {
+                if (ch == 'a') result.Append('e');
+                else if (ch == 'e') result.Append('i');
+                else if (ch == 'i') result.Append('o');
+                else if (ch == 'o') result.Append('u');
+                else if (ch == 'u') result.Append('a');
+                else
+                {
+                    char next = (char)(ch + 1);
+
+                    if (next > 'z')
+                        next = 'a';
+
+                    if ("aeiou".Contains(next))
+                    {
+                        next++;
+                        if (next > 'z')
+                            next = 'a';
+                    }
+
+                    result.Append(next);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter lowercase string:");
+            string input = Console.ReadLine();
+
+            vowelShiftingCipher obj = new vowelShiftingCipher();
+            string output = obj.VowelShiftCipher(input);
+
+            Console.WriteLine("Output: " + output);
+        }
+    }
+}
