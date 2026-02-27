2af7190c9dec212ea72e532433556d531ef16a58
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-02-27 14:42:40 +0530

    DAY 4

---
+++ Exercises/Day4-Project/Controllers/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingPortal.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult MyContact()
+        {
+            return View();
+        }
+    }
+}
