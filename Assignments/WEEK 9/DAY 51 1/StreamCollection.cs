c6366d9d67966dfa847b8189a13ad0ab89f9d7be
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-03-07 10:21:05 +0530

    Exercise added

---
+++ Assignments/WEEK 9/DAY 51 1/StreamCollection.cs
@@ -0,0 +1,17 @@
+﻿namespace Week09
+{
+    public class CreatorStats
+    {
+        public string CreatorName { get; set; }
+        public double[]  WeeklyLikes { get; set; }
+
+        public List<>
+    }
+    internal class StreamCollection
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
