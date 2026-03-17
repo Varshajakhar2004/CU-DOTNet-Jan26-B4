96524ddf37fc29fe36191e444254dc56b20e8981
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-03-17 14:28:18 +0530

    done

--- Weekly Assessments/Week 10 Assessment/FinTrackPro/Controllers/HomeController.cs
+++ Weekly Assessments/Week 10 Assessment/FinTrackPro/Controllers/HomeController.cs
@@ -1,21 +1,40 @@
 using System.Diagnostics;
 using FinTrackPro.Models;
 using Microsoft.AspNetCore.Mvc;
+using FinTrackPro.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinTrackPro.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly FinTrackProContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, FinTrackProContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var vm = new DashboardViewModel();
+
+            vm.TotalAccounts = _context.Account.Count();
+            vm.TotalBalance = _context.Account.Sum(a => a.Balance);
+
+            var allTx = _context.Transaction
+                        .Include(t => t.Account)
+                        .AsNoTracking()
+                        .OrderByDescending(t => t.Date)
+                        .ToList();
+            vm.TotalCredit = allTx.Where(t => (t.Category ?? "").Equals("Credit", System.StringComparison.OrdinalIgnoreCase)).Sum(t => t.Amount);
+            vm.TotalDebit = allTx.Where(t => (t.Category ?? "").Equals("Debit", System.StringComparison.OrdinalIgnoreCase)).Sum(t => t.Amount);
+
+            vm.RecentTransactions = allTx.Take(10).ToList();
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
